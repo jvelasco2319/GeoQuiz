4a336fcad9770dafb4e0d3cf137850628e99d2f2
rkjc <jvelasco2319@yahoo.com>
2017-09-27 13:15:49 -0700

    added listeners

--- app/src/main/java/com/example/james/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/james/geoquiz/QuizActivity.java
@@ -4,19 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.MenuItemHoverListener;
 import android.view.Menu;
+import android.view.View;
 import android.widget.Button;
 
 import static com.example.james.geoquiz.R.styleable.MenuItem;
 
+//appCompatActivity is a subclass of androids activity class that provides compatibility
+//support for older versions of android
 public class QuizActivity extends AppCompatActivity {
 
+    //create variables for the buttons
     private Button mTrueButton;
     private Button mFalseButton;
 
+    //this bundle is called whenan instance of the activity subclass is created
+    //when activity is created, needs a UI to manage.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setcontentview calls the activity_quiz layout
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+
+        //this onclickistener waits until the button is pressed
+        //and then it does the action within the public void onClick
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //does nothing yet
+            }
+        });
+
+
+
+
+        mFalseButton = (Button) findViewById(R.id.false_button);
+
     }
 
 }
