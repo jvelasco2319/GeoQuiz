1a19c331658069633a56b79b485879f53ad543b1
jvelasco2319 <jvelasco2319@yahoo.com>
2017-09-26 21:08:35 -0700

    9/26/17

--- app/src/main/java/com/example/james/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/james/geoquiz/QuizActivity.java
@@ -2,12 +2,39 @@ package com.example.james.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.MenuItemHoverListener;
+import android.view.Menu;
+import android.widget.Button;
+
+import static com.example.james.geoquiz.R.styleable.MenuItem;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
     }
+
+}
+
+    /*
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.quiz, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+/*
 }
